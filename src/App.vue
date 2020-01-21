9e224cd171333a5bbcca463f592862499f4c2ef6
King-Mike <nsubugamike021@gmail.com>
2020-01-21 17:05:21 +0300

    Changed es-lint configs

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,21 @@
 </template>
 
 <script>
+/* eslint-disable */
 import Layout from "./components/Layout/Layout";
+import axios from 'axios'
 
 export default {
   name: "app",
   components: {
     Layout
+  },
+  created(){
+    axios.get("https://jsonplaceholder.typicode.com/todos/1")
+    .then(response => console.log("Data: ",response.data))
+    .catch(err => console.log("Error:", err))
+
+    
   }
 };
 </script>
--- src/components/Main/Main.vue
+++ src/components/Main/Main.vue
@@ -1,11 +1,39 @@
 <template>
-  <div class="col-md-9 ml-sm-auto col-lg-10 px-4">Main Section</div>
+<div>
+<div class="col-md-9 ml-sm-auto col-lg-10 px-4">Main Section
+    {{msg}}
+  </div>
+</div>
+
 </template>
 
 <script>
 export default {
-  name: "Main"
-};
+  name: "Main",
+
+data(){
+  return{
+    msg:'Hello',
+    todos:[
+      {
+        id:1,
+        title:"Todo One",
+        completed:false
+      },
+      {
+        id:2,
+        title: "Todo two",
+        completed: true
+      },
+      {
+        id:3,
+        title:"Todo three",
+        completed:false
+      }
+    ]
+  }
+}
+}
 </script>
 
 <style>
