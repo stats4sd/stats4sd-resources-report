815887b9f7bc5da9f43600b71fa907d61466903c
KingMike100 <nsubugamike021@gmail.com>
2020-01-28 18:32:11 +0300

    Final tidying with all changes

--- src/components/Main/Main.vue
+++ src/components/Main/Main.vue
@@ -5,35 +5,33 @@
 </template>
 
 <script>
-/* eslint-disable */
-import GoogleAnalytics from '../GoogleAnalytics/GoogleAnalytics'
+import GoogleAnalytics from '../GoogleAnalytics/GoogleAnalytics';
 export default {
-  name: "Main",
-  components: {GoogleAnalytics},
-data(){
-  return{
-    msg:'Hello',
-    todos:[
-      {
-        id:1,
-        title:"Todo One",
-        completed:false
-      },
-      {
-        id:2,
-        title: "Todo two",
-        completed: true
-      },
-      {
-        id:3,
-        title:"Todo three",
-        completed:false
-      }
-    ]
+  name: 'Main',
+  components: { GoogleAnalytics },
+  data() {
+    return {
+      msg: 'Hello',
+      todos: [
+        {
+          id: 1,
+          title: 'Todo One',
+          completed: false
+        },
+        {
+          id: 2,
+          title: 'Todo two',
+          completed: true
+        },
+        {
+          id: 3,
+          title: 'Todo three',
+          completed: false
+        }
+      ]
+    };
   }
-}
-}
+};
 </script>
 
-<style>
-</style>
+<style></style>
