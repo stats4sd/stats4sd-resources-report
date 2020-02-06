0533f3669ee56bc2caccdb2b4593294d59a3df6c
KingMike100 <nsubugamike021@gmail.com>
2020-02-06 16:42:59 +0300

    Fetched resources using axios

--- src/components/GoogleAnalytics/Resources.vue
+++ src/components/GoogleAnalytics/Resources.vue
@@ -2,8 +2,25 @@
   <div>Resources</div>
 </template>
 <script>
+import axios from 'axios';
 export default {
-  name: 'Resources'
+  name: 'Resources',
+  data() {
+    return {
+      troves: []
+    };
+  },
+  mounted: function() {
+    axios
+      .get('http://localhost:3000/troves')
+      .then(response => {
+        this.troves = response.data;
+        console.log('Troves data', this.troves);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
 };
 </script>
 <style scoped></style>
