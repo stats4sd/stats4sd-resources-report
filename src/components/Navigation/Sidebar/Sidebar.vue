10de336cd58ace54baef52804114a3c4f2cdf371
KingMike100 <nsubugamike021@gmail.com>
2020-02-17 11:12:06 +0300

    Removed Dist folder

--- src/components/Navigation/Sidebar/Sidebar.vue
+++ src/components/Navigation/Sidebar/Sidebar.vue
@@ -4,17 +4,13 @@
       <div class="sidebar-sticky">
         <ul class="nav flex-column">
           <li class="nav-item">
-            <router-link to="/" class="nav-link">Dashboard</router-link>
+            <router-link to="/" class="nav-link">Website</router-link>
           </li>
           <li class="nav-item">
-            <router-link to="/resources" class="nav-link"
-              >Resources</router-link
-            >
+            <router-link to="/resources" class="nav-link">Resources</router-link>
           </li>
           <li class="nav-item">
-            <router-link to="/collections" class="nav-link"
-              >Collections</router-link
-            >
+            <router-link to="/collections" class="nav-link">Collections</router-link>
           </li>
         </ul>
       </div>
