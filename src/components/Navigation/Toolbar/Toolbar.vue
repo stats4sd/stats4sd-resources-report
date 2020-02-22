593b09d7af60f72fd83aa6381c69683a3c63ad4c
KingMike100 <nsubugamike021@gmail.com>
2020-02-22 21:08:42 +0300

    Added toggler to navbar on small mobile

--- src/components/Navigation/Toolbar/Toolbar.vue
+++ src/components/Navigation/Toolbar/Toolbar.vue
@@ -1,24 +1,55 @@
 <template>
-  <div>
-    <nav class="navbar navbar-expand-lg navbar-light mb-4">
-      <router-link class="navbar-brand" to="/">
-        <img
-          src="https://stats4sd.org/img/stats4sd.png"
-          width="100px"
-          alt="stats4sd"
-        />
-      </router-link>
-
-      <div id="navbarNavAltMarkup" class="collapse navbar-collapse">
-        <div class="navbar-nav"></div>
-      </div>
-    </nav>
-  </div>
+  <nav
+    class="navbar navbar-expand-lg navbar-light bg-light"
+    :class="{ navbarOpen: show }"
+  >
+    <router-link class="navbar-brand" to="/">
+      <img
+        src="https://stats4sd.org/img/stats4sd.png"
+        width="100px"
+        alt="stats4sd"
+      />
+    </router-link>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      @click.stop="toggleNavbar()"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div
+      id="navbarSupportedContent"
+      class="collapse navbar-collapse"
+      :class="{ show: show }"
+    >
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <router-link class="nav-link" to="/"
+            >Website <span class="sr-only">(current)</span></router-link
+          >
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/resources">Resources</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
-
 <script>
 export default {
-  name: 'Toolbar'
+  name: 'Toolbar',
+  data() {
+    return {
+      show: true
+    };
+  },
+  methods: {
+    toggleNavbar() {
+      this.show = !this.show;
+    }
+  }
 };
 </script>
 
