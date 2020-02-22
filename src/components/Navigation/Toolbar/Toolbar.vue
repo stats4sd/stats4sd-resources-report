1eca4243d0b2783c7e550102c980c8c3a5915d45
KingMike100 <nsubugamike021@gmail.com>
2020-02-22 17:20:20 +0300

    Remove Toggler Button

--- src/components/Navigation/Toolbar/Toolbar.vue
+++ src/components/Navigation/Toolbar/Toolbar.vue
@@ -2,17 +2,7 @@
   <div>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <router-link class="navbar-brand" to="/">Stats4SD</router-link>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
+
       <div id="navbarNavAltMarkup" class="collapse navbar-collapse">
         <div class="navbar-nav"></div>
       </div>
