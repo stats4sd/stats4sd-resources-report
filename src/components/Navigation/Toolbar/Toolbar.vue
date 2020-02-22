b43cf2164a41ba9926648e886cb94e5779e640b7
KingMike100 <nsubugamike021@gmail.com>
2020-02-22 19:18:27 +0300

    Responsive screen on small screens

--- src/components/Navigation/Toolbar/Toolbar.vue
+++ src/components/Navigation/Toolbar/Toolbar.vue
@@ -2,7 +2,11 @@
   <div>
     <nav class="navbar navbar-expand-lg navbar-light mb-4">
       <router-link class="navbar-brand" to="/">
-        <img src="https://stats4sd.org/img/stats4sd.png" width="100px" alt="stats4sd" />
+        <img
+          src="https://stats4sd.org/img/stats4sd.png"
+          width="100px"
+          alt="stats4sd"
+        />
       </router-link>
 
       <div id="navbarNavAltMarkup" class="collapse navbar-collapse">
@@ -18,4 +22,10 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+@media screen and (min-width: 992px) {
+  .navbar {
+    display: none;
+  }
+}
+</style>
